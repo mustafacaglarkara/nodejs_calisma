0a6830252da47efafc5169d1e2d5fc67141d0d6c
Mustafa Çağlar KARA <mustafacaglarkara@gmail.com>
2022-03-12 22:57:54 +0300

    Create array_iter.js

---
+++ array_iter.js
@@ -0,0 +1,17 @@
+const numbers = [2,55,9,59,5,69,8,78];
+
+// for iter index
+for(let index=0; index<numbers.length; index++){
+    console.log(numbers[index]);
+}
+
+// for iter in
+for(let item in numbers){
+    console.log(numbers[item]);
+}
+//for iter of
+for(let item of numbers){
+    console.log(item);
+}
+
+console.log(numbers);
